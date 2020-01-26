425c32ec438794187de21a61eab260e59ca76cb1
yeqifu <1784525940@qq.com>
2020-01-26 22:02:57 +0800

    初始化环境

---
+++ src/main/java/com/yeqifu/WarehouseBackApplication.java
@@ -0,0 +1,13 @@
+package com.yeqifu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseBackApplication.class, args);
+    }
+
+}
